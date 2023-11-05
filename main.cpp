9582cd7c7d2e25ec3c955cafe3fdd1ebe6991e72
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-05 12:02:20 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-#include<iostream>
 
-int main()
-{
+#include <iostream>
 
-	return 0;
+int main() {
+    int a, b;
+
+
+    std::cout << "Enter the first number (a): ";
+    std::cin >> a;
+    std::cout << "Enter the second number (b): ";
+    std::cin >> b;
+
+    int* ptrA = &a;
+    int* ptrB = &b;
+
+
+    int temp = *ptrA;
+    *ptrA = *ptrB;
+    *ptrB = temp;
+
+    std::cout << "After swapping:\n";
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
+    return 0;
 }
