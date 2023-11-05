1f1fe6dcd0ccce36ec905bdb4272a9c23efa6ce0
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-05 12:58:39 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <ctime> 
 
-int main()
-{
+int main() {
+    const int arraySize = 5;
+    int numbers[arraySize];
 
-	return 0;
+   
+    srand(static_cast<unsigned int>(time(0)));
+
+   
+    for (int i = 0; i < arraySize; i++) {
+        numbers[i] = std::rand() % 100 + 1; 
+    }
+
+    int sum = 0;
+
+   
+    int* ptr = numbers; 
+
+    for (int i = 0; i < arraySize; i++) {
+        sum += *ptr;
+        ptr++;
+    }
+
+    std::cout << "Randomly generated array: ";
+    for (int i = 0; i < arraySize; i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << "\nSum of array elements: " << sum << std::endl;
+
+    return 0;
 }
