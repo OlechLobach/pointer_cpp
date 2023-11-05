3ec70922481cdf00774a0941943bdf5903a20ec7
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-05 14:04:00 +0200

    штшешфд сщььше

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
+using namespace::std;
 
-int main()
-{
+const int arrSize = 5;
 
-	return 0;
+int main() {
+    int sourceArr[arrSize];
+    int destinationArr[arrSize];
+
+    
+    srand(static_cast<unsigned int>(time(0)));
+
+   
+    for (int i = 0; i < arrSize; i++) {
+        sourceArr[i] = rand() % 100; 
+    }
+
+    
+    cout << "Init array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << sourceArr[i] << " ";
+    }
+    cout << endl;
+
+    
+    for (int i = 0; i < arrSize; i++) {
+        destinationArr[i] = sourceArr[arrSize - 1 - i];
+    }
+
+    cout << "Copied array after reverse: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << destinationArr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
