9e71261e4b7520116db2bea046e9c7d33425fc11
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-05 11:22:13 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -3,5 +3,24 @@
 int main()
 {
 
+    int num1, num2;
+
+    
+    std::cout << "Enter the first nimber: ";
+    std::cin >> num1;
+
+    
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+ 
+    int* ptr1 = &num1;
+    int* ptr2 = &num2;
+
+    
+    int largest = (*ptr1 > *ptr2) ? *ptr1 : *ptr2;
+
+    std::cout << "The Largest number " << largest << std::endl;
+
 	return 0;
 }
