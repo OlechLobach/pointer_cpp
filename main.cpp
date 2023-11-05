d9009bd3c41e00ba686c0e0aa65d6b0e39b0e2aa
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-05 12:44:00 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include<iostream>
-
+using namespace ::std;
 int main()
 {
+	double num1, num2, result;
+	char operation;
+	cout << "Enter the first number:";
+	cin >> num1;
+	cout << "Enter the operation (+, -, *, /):";
+	cin >> operation;
+	cout << "Enter the second number:";
+	cin >> num2;
 
+	double* ptr1 = &num1;
+	double* ptr2 = &num2;
+
+	if (operation == '+') {
+		result = *ptr1 + *ptr2;
+	}
+	else if (operation == '-') {
+		result = *ptr1 - *ptr2;
+	}
+	else if (operation == '*') {
+		result = *ptr1 * *ptr2;
+	}
+	else if (operation == '/') {
+		if (*ptr2 != 0) {
+			result = *ptr1 / *ptr2;
+		}
+		else {
+			cout << "division by zero error " << endl;
+			return 1;
+		}
+	}
+	else {
+		cout << "unknown operation." << endl;
+	}
+	cout << "result:" << *ptr1 << "" << operation << "" << *ptr2 << "=" << result << endl;
 	return 0;
 }
