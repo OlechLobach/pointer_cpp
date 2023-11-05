85bffdc847af787d94d093ff5ae417ed89859063
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-05 11:42:58 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 int main()
 {
+    int number;
+    std::cout << "Enter the number: ";
+    std::cin >> number;
 
+    int* pointer = &number; 
+
+    if (*pointer > 0) {
+        std::cout << "The number is positive." << std::endl;
+    }
+    else if (*pointer < 0) {
+        std::cout << "The number is negative." << std::endl;
+    }
+    else {
+        std::cout << "The number is equal to zero." << std::endl;
+    }
 	return 0;
 }
