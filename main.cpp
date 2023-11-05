909511f51b237a45d1f1bdde111d67f445ee8c42
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-05 13:54:37 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,51 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
+using namespace::std;
 
-int main()
-{
+const int arrSize = 5;
 
-	return 0;
+
+int generateRandomNumber(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
+void generateRandomArr(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = generateRandomNumber(0, 99);  
+    }
+}
+
+int main() {
+    int myArr[arrSize];
+
+ 
+    srand(static_cast<unsigned int>(time(0)));
+
+    generateRandomArr(myArr, arrSize);
+
+    cout << "Init array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << myArr[i] << " ";
+    }
+    cout << std::endl;
+
+    int* start = myArr;
+    int* end = myArr + arrSize - 1;
+
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+
+
+    cout << "The array after reversing: ";
+    for (int i = 0; i < arrSize; i++) {
+       cout << myArr[i] << " ";
+    }
+    cout << std::endl;
+
+    return 0;
 }
