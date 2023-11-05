c269a6b3c97e0e2ac5c4c2430707688214e1fdd9
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-05 13:28:39 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
-#include<iostream>
+#include <iostream>
+#include <ctime>
+using namespace::std;
+const int arrSize = 5;
 
-int main()
-{
+void copyArr(int* source, int* destination, int size) {
+    for (int i = 0; i < size; i++) {
+        *destination = *source;
+        source++;     
+        destination++;
+    }
+}
 
-	return 0;
+int main() {
+    int sourceArr[arrSize];
+    int destinationArr[arrSize];
+
+
+    srand(static_cast<unsigned int>(time(0)));
+
+   
+    for (int i = 0; i < arrSize; i++) {
+        sourceArr[i] = rand() % 100; 
+    }
+
+ 
+    cout << "Source Array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << sourceArr[i] << " ";
+    }
+    cout << endl;
+
+
+    copyArr(sourceArr, destinationArr, arrSize);
+
+    cout << "Copied Array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << destinationArr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
